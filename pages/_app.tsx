7cb32440b402b3c445bc3679bc5ff3a907b16fd8
Emir Amanbekov <amanbekoff@gmail.com>
2023-03-04 12:07:20 +0300

    Add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,24 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-PNY43DLBWL"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-PNY43DLBWL');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  )
 }
